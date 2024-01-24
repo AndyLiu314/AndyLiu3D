fa8ec6d0e59ea93d73d04d681e687faa69d41c49
Andy Liu <andyliu4002@gmail.com>
2024-01-23 18:41:32 -0800

    Added camera rig file and setup the canvas

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center } from '@react-three/drei'
+
+import CameraRig from './CameraRig'
 
 const CanvasModule = () => {
   return (
-    <div>Canvas</div>
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset='city' />
+      
+      <CameraRig>
+        <Center>
+          {/* PUT .JSX FILE FROM CANVAS FOLDER THAT HAS THREE JS SHIT */}
+        </Center>
+      </CameraRig>
+    </Canvas>
   )
 }
 
