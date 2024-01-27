8a54080b94794c94a67158b0303551f92db5de4d
Andy Liu <andyliu4002@gmail.com>
2024-01-27 11:28:37 -0800

    updated navbar to use navlink

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className='header'>
+      <NavLink>
+
+      </NavLink>
+
+    </header>
   )
 }
 
