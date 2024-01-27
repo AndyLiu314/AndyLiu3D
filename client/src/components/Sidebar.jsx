a58d7c1f35bc2045d9af673c3318a39e5be579c7
Andy Liu <andyliu4002@gmail.com>
2024-01-27 11:56:17 -0800

    Added icon assets and sidebar function for icon use

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -3,12 +3,18 @@ import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
-    <header className='header'>
+    <div className='fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg'>
       <NavLink to = '/' className = ' '>
 
       </NavLink>
-    </header>
+    </div>
   )
 }
+
+const SidebarIcon = ({ icon }) => (
+  <div className='sidebar-icon'>
+    {icon}
+  </div>
+)
 
 export default Sidebar
