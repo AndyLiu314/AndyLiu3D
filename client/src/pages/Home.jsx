d2ad06da8fa59917079745ec89cd00b1de1a1de2
Andy Liu <andyliu4002@gmail.com>
2024-01-31 19:41:41 -0800

    Updated camera position
    Camera can now see rocket better, added orbit controls for fun

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,12 +3,13 @@ import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Loader from '../components/Loader';
 import Rocket from '../models/Rocket';
+import { OrbitControls } from '@react-three/drei';
 
 const Home = () => {
   const adjustRocket = () => {
     let screenScale = null;
-    let screenPosition = [0, -6.5, -43];
-    let rotation = [0,0,0];
+    let screenPosition = [0, -20, 0];
+    let rotation = [0,3.15,0];
 
     if (window.innerWidth < 768) {
       screenScale = [0.9, 0.9, 0.9];
@@ -25,9 +26,14 @@ const Home = () => {
     <section className = 'w-full h-screen absolute'>
       <Canvas 
         className='w-full h-screen bg-transparent'
-        camera = {{ near: 0.1, far: 1000}} >
+        camera = {{ 
+          fov: 75,
+          near: 0.1, 
+          far: 1000,
+          position: [0, 0, -40]}} >
 
         <Suspense fallback={<Loader />}>
+          {/* <OrbitControls /> */}
           <directionalLight />
           <ambientLight />
           <pointLight />
