263f7572124ff861d2f65b5d48d730d2639a2cba
Andy Liu <andyliu4002@gmail.com>
2024-02-01 16:12:10 -0800

    Moved navbar to left side
    small changes to text colour to see it better

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -7,18 +7,18 @@ import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
-    <header className='header'>
+    <header className='fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg'>
       <NavLink to = '/' className = 'w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md'>
         <p className='blue-gradient_text'>AL</p>
       </NavLink>
       <nav className='flex text-lg gap-7 font-medium'> 
-        <NavLink to='/about' className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
+        <NavLink to='/about' className={({ isActive }) => isActive ? 'text-blue-500' : 'text-green-500'}>
           About
         </NavLink>
-        <NavLink to='/projects' className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
+        <NavLink to='/projects' className={({ isActive }) => isActive ? 'text-blue-500' : 'text-green-500'}>
           Projects
         </NavLink>
-        <NavLink to='/contact' className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
+        <NavLink to='/contact' className={({ isActive }) => isActive ? 'text-blue-500' : 'text-green-500'}>
           Contact
         </NavLink>
       </nav>
