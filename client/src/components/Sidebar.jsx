9469345fcb215e141058934e1ed5db76c8ed7bdd
Andy Liu <andyliu4002@gmail.com>
2024-02-07 15:17:49 -0800

    Installed svgr
    Navlink doesnt seem to allow adding img tag or other react component in the inner html. Will keep trying to add icons.

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Saturn from '../assets/Icons/Planets/saturn-svg.svg'
 
 /* SIDEBAR IMPLEMENTATION     
 <div className='fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg'>
@@ -13,6 +14,7 @@ const Sidebar = () => {
       </NavLink>
       <NavLink to='/about' className={({ isActive }) => isActive ? 'text-blue-500' : 'text-green-500'}>
         About
+        {/* <Saturn /> */}
       </NavLink>
       <NavLink to='/projects' className={({ isActive }) => isActive ? 'text-blue-500' : 'text-green-500'}>
         Projects
