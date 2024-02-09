f372c37eef5831211d26407941fa3ae37d4f5ea1
Andy Liu <andyliu4002@gmail.com>
2024-02-09 13:58:52 -0800

    Implemented svg icons to sidebar tabs

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Saturn from '../assets/Icons/Planets/saturn-svg.svg'
+import Earth from '../assets/Icons/Planets/planet-earth-svg.svg'
+import Mars from '../assets/Icons/Planets/mars-svg.svg'
+import Jupiter from '../assets/Icons/Planets/jupiter-svg.svg'
 
 /* SIDEBAR IMPLEMENTATION     
 <div className='fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg'>
@@ -10,18 +13,16 @@ const Sidebar = () => {
   return (
     <header className='header'>
       <NavLink to = '/' className={({ isActive }) => isActive ? 'text-blue-500' : 'text-green-500'}>
-        AL
+        <SidebarIcon icon={Earth} />
       </NavLink>
       <NavLink to='/about' className={({ isActive }) => isActive ? 'text-blue-500' : 'text-green-500'}>
-        About
-        {/* <Saturn /> */}
-        {/* <img /> */}
+        <SidebarIcon icon={Mars} />
       </NavLink>
       <NavLink to='/projects' className={({ isActive }) => isActive ? 'text-blue-500' : 'text-green-500'}>
-        Projects
+        <SidebarIcon icon={Jupiter} />
       </NavLink>
       <NavLink to='/contact' className={({ isActive }) => isActive ? 'text-blue-500' : 'text-green-500'}>
-        Contact
+      <SidebarIcon icon={Saturn} />
       </NavLink>
     </header>
   )
@@ -29,7 +30,7 @@ const Sidebar = () => {
 
 const SidebarIcon = ({ icon }) => (
   <div className='sidebar-icon'>
-    {icon}
+    <img src={icon} />
   </div>
 )
 
