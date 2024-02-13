991dc28caaef233d8868c77ba359cea34f28e6f6
Andy Liu <andyliu4002@gmail.com>
2024-02-12 18:59:42 -0800

    Wrote addStars function
    function generates stars in random positions over the canvas

--- client/src/models/Space.jsx
+++ client/src/models/Space.jsx
@@ -1,5 +1,6 @@
 import { useGLTF } from '@react-three/drei';
 import React from 'react';
+import * as THREE from 'three';
 
 import spaceScene from '../assets/3D/stars_skybox.glb'
 
@@ -12,6 +13,23 @@ const Space = () => {
           scale = {[100, 100, 100]} />
     </mesh>
   )
+}
+
+function addStars() {
+  
+  return (
+    <mesh>
+      <sphereGeometry 
+        args={[0.25, 24, 24]}
+        position={[1,1,1]}/>
+      <meshStandardMaterial />
+    </mesh>
+  )
+}
+
+function getRandomPosition() {
+  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ));
+  return [x,y,z];
 }
 
 export default Space;
