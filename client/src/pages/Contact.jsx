9e0694fd26676edba0ec99f47d46c5d8326b990a
Andy Liu <andyliu4002@gmail.com>
2024-03-05 14:28:24 -0800

    Update Contact page to have space

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import Loader from '../components/Loader';
+import { Canvas } from '@react-three/fiber';
+import React, { Suspense } from 'react';
+import Space from '../models/Space';
+import { OrbitControls } from '@react-three/drei';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className='w-screen h-screen fixed'>
+      <Canvas
+        className='w-screen h-screen bg-transparent'
+        camera={{ 
+          fov: 60,
+          near: 0.1, 
+          far: 1000,
+          position: [0, 0, -25]}}>
+        <Suspense fallback={<Loader />}>
+          <OrbitControls />
+          <directionalLight position={[1,1,1]} intensity={0}/>
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+          <Space />
+        </Suspense>
+      </Canvas>
+    </section>
   )
 }
 
