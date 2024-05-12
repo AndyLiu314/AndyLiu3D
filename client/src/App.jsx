4b12cf94bc55478748c5eb6e46b9732b571dfc79
Andy Liu <andyliu4002@gmail.com>
2024-05-11 18:43:50 -0700

    Cleaned up App.jsx
    * Removed commented out code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,6 @@ import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 import {Home, About, Projects, Contact} from './pages';
 import './App.css';
 
-/* TESTING THREE.JS SCENE 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#three-canvas'),
-});
-
-renderer.setPixelRatio(window.devicePixelRatio);
-//renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setSize(800,600);
-camera.position.setZ(30);
-renderer.render(scene, camera); */
-
 function App() {
   return (
     <main>
